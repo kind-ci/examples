5e95713288dd8f54aadd17343bcf870e0818a914
Benjamin Elder <bentheelder@google.com>
2019-05-09 12:36:19 -0700

    add circle-ci

---
+++ kind-smoketest.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# standard bash error handling
+set -o errexit;
+set -o pipefail;
+set -o nounset;
+# debug commands
+set -x;
+
+# working dir to install binaries etc, cleaned up on exit
+BIN_DIR="$(mktemp -d)"
+# kind binary will be here
+KIND="${BIN_DIR}/kind"
+
+# cleanup on exit (useful for running locally)
+cleanup() {
+    "${KIND}" delete cluster || true
+    rm -rf "${BIN_DIR}"
+}
+trap cleanup EXIT
+
+# util to install the latest kind version into ${BIN_DIR}
+install_latest_kind() {
+    # clone kind into a tempdir within BIN_DIR
+    local tmp_dir
+    tmp_dir="$(TMPDIR="${BIN_DIR}" mktemp -d "${BIN_DIR}/kind-source.XXXXX")"
+    cd "${tmp_dir}" || exit
+    git clone https://github.com/kubernetes-sigs/kind && cd ./kind
+    make install INSTALL_DIR="${BIN_DIR}"
+}
+
+# util to install a released kind version into ${BIN_DIR}
+install_kind_release() {
+    VERSION="0.2.1"
+    KIND_BINARY_URL="https://github.com/kubernetes-sigs/kind/releases/download/${VERSION}/kind-linux-amd64"
+    wget -O "${KIND}" "${KIND_BINARY_URL}"
+    chmod +x "${KIND}"
+}
+
+main() {
+    # get kind
+    install_latest_kind
+    # create a cluster
+    "${KIND}" create cluster --loglevel=debug
+    # set KUBECONFIG to point to the cluster
+    KUBECONFIG="$("${KIND}" get kubeconfig-path)"
+    export KUBECONFIG
+    # TODO: invoke your tests here
+    # teardown will happen automatically on exit
+}
+
+main
